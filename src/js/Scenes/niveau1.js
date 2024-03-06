adbc2b12faaa6cd3aa3457bbed967064700b7199
Your Name <you@example.com>
2024-03-06 23:48:15 +0100

    modifications de la map du niveau1, ajout d'ennemis et nouvelles animations

--- src/js/Scenes/niveau1.js
+++ src/js/Scenes/niveau1.js
@@ -6,6 +6,7 @@ import Character from "/src/js/Beings/character.js";
 
 // création et lancement du jeu
 var ice;
+var porte_ouvrante1;
 
 export default class niveau1 extends Phaser.Scene {
     // constructeur de la classe
@@ -23,7 +24,7 @@ export default class niveau1 extends Phaser.Scene {
         this.load.image("Phaser_tuilesdejeu2", "src/assets/neige.png");
 
 
-        this.load.tilemapTiledJSON("Ice", "src/assets/niveauIce.json");
+        this.load.tilemapTiledJSON("Iced", "src/assets/niveauIced.json");
     }
 
     create() {
@@ -32,7 +33,7 @@ export default class niveau1 extends Phaser.Scene {
         this.boundWidth = 3200;
         this.boundHeight = 3200;
 
-        const carteDuNiveau = this.add.tilemap("Ice");
+        const carteDuNiveau = this.add.tilemap("Iced");
         const tileset = carteDuNiveau.addTilesetImage("snow", "Phaser_tuilesdejeu");
         const tileset1 = carteDuNiveau.addTilesetImage("ice", "Phaser_tuilesdejeu1");
         const tileset2 = carteDuNiveau.addTilesetImage("neige", "Phaser_tuilesdejeu2");
@@ -45,6 +46,11 @@ export default class niveau1 extends Phaser.Scene {
         const object = carteDuNiveau.createLayer("object", [tileset,tileset1,tileset2]);
         const ice = carteDuNiveau.createLayer("ice", [tileset,tileset1,tileset2]);
         ice.setCollisionByProperty({ estSolide: true });
+
+
+        this.porte_ouvrante1 = this.physics.add.staticSprite(560, 580, "porte_ouvrante"); 
+        this.porte_ouvrante1.ouverte = false; 
+
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -62,32 +68,39 @@ export default class niveau1 extends Phaser.Scene {
         this.weap = new Range(this, "bull", 2, 10, 1, "bullet", true, 1, 1000, false);
         this.player.pickWeapon(this.weap);
 
-        //const tab_points = carteDuNiveau.getObjectLayer("calque_ennemis");
+        const tab_points = carteDuNiveau.getObjectLayer("calque_ennemis");
 
-     //   this.groupe_ennemis = this.physics.add.group();
+        this.groupe_ennemis = this.physics.add.group();
 
-       // this.physics.add.collider(this.groupe_ennemis, calque_plateformes);
-this.physics.world.setBounds(this.boundx,this.boundy,this.boundWidth,this.boundHeight)
-       // tab_points.objects.forEach(point => {
-           // if (point.name == "ennemi") {
-            //    var nouvel_ennemi = new Terrestre(this, "img_perso", point.x, point.y, calque_plateformes);
-            //    nouvel_ennemi.sprite.ennemiObject = nouvel_ennemi;
-            //    this.groupe_ennemis.add(nouvel_ennemi.sprite);
-          //  }
-      //  });
+        this.physics.add.collider(this.groupe_ennemis, ice);
+        this.physics.world.setBounds(this.boundx,this.boundy,this.boundWidth,this.boundHeight)
+        tab_points.objects.forEach(point => {
+            if (point.name == "ennemi_sol") {
+                var nouvel_ennemi = new Terrestre(this, "img_perso", point.x, point.y, ice);
+                nouvel_ennemi.sprite.ennemiObject = nouvel_ennemi;
+               this.groupe_ennemis.add(nouvel_ennemi.sprite);
+           }
+        });
+        tab_points.objects.forEach(point => {
+            if (point.name == "ennemi_air") {
+                var nouvel_ennemi = new Flying(this, "img_perso", point.x, point.y, ice);
+                nouvel_ennemi.sprite.ennemiObject = nouvel_ennemi;
+               this.groupe_ennemis.add(nouvel_ennemi.sprite);
+           }
+        });
 
         this.player.inventory.forEach(element => {
             this.physics.add.collider(element.Bullets, ice, element.erase, null, element);
-          //  this.physics.add.overlap(element.Bullets, this.groupe_ennemis, element.hit, null, element);
+            this.physics.add.overlap(element.Bullets, this.groupe_ennemis, element.hit, null, element);
         });
 
         this.physics.add.collider(this.player.sprite, this.groupe_ennemis, this.handlePlayerEnnemiCollision, null, this);
 
-       // this.groupe_ennemis.children.iterate(function (un_ennemi, iterateur) {
-        //    un_ennemi.setVelocityX(-90);
-        //    un_ennemi.direction = "left";
-        //    un_ennemi.anims.play("turn_left", true);
-      //  });
+        this.groupe_ennemis.children.iterate(function (un_ennemi, iterateur) {
+            un_ennemi.setVelocityX(-90);
+           un_ennemi.direction = "left";
+            un_ennemi.anims.play("turn_left", true);
+        });
 
     }
 
@@ -113,9 +126,22 @@ this.physics.world.setBounds(this.boundx,this.boundy,this.boundWidth,this.boundH
             this.time.delayedCall(3000, this.restartScene, [], this);
         }
 
-      //  this.groupe_ennemis.children.iterate(function (un_ennemi, iterateur) {
-       //     un_ennemi.ennemiObject.update();
-       // });
+        if ( Phaser.Input.Keyboard.JustDown(this.cursors.space) == true &&
+        this.physics.overlap(this.player.sprite, this.porte_ouvrante) == true) {
+       // le personnage est sur la porte et vient d'appuyer sur espace
+       if (this.porte_ouvrante.ouverte == false) {
+        this.porte_ouvrante.anims.play("anim_ouvreporte");
+        this.porte_ouvrante.ouverte = true;
+        this.scene.start("niveau2");
+      } else {
+        this.porte_ouvrante.anims.play("anim_fermeporte");
+        this.porte_ouvrante.ouverte = false;
+      }
+      } 
+
+        this.groupe_ennemis.children.iterate(function (un_ennemi, iterateur) {
+            un_ennemi.ennemiObject.update();
+        });
     }
 
     restartScene() {
