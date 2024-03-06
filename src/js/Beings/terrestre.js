3e67790c45a9cf7511de939b5af16dfb51de1fba
Evan ROGIER <evan.rogier@epfedu.fr>
2024-03-06 13:33:18 +0100

    intégration des assets + modifications de la map du niveau 4

--- src/js/Beings/terrestre.js
+++ src/js/Beings/terrestre.js
@@ -34,7 +34,7 @@ export default class Terrestre extends Ennemi{
                     this.sprite.setVelocityX(-this.speedx);
                     this.sprite.anims.play("turn_left", true);
                 }
-            }
+            }   
         }
     }
 }
